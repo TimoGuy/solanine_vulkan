df3addb0c7556eda6024ecce9ebedcf76593d9c9
TimoGuy <timoguy99004@gmail.com>
2022-11-09 13:59:10 -0800

    Improves build time by ~6 seconds for debug config... but slimegirl.glb takes ~1 second longer to load

---
+++ solanine_vulkan/src/pch.cpp
@@ -0,0 +1,4 @@
+#include "pch.h"
+
+// @CHECK: do we even need there to be a .cpp file that is used to create the pch? Why can't pch.h be the create file?
+// @ANSWER: you can't set this settings on a .h file bc it never gets compiled.... it's just the design of the compiler eh!
