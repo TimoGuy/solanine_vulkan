ad7b2897c2ee91d766d81478481731478314cef4
TimoGuy <timoguy99004@gmail.com>
2023-02-06 07:51:45 -0800

    Adds some info on dungeon system with its interaction with overworld

--- etc/world_gen_alg_poc/dungeon_worldgen.py
+++ etc/world_gen_alg_poc/dungeon_worldgen.py
@@ -14,15 +14,31 @@ There are multiple types of dungeon I want to generate:
     - Many different choices, and manipulating structures to open up backtracking and choice
 
 
+There will be at least 1 village in the biome that will help you get the skill check required to get into the dungeon.
+
+Auxiliary villages will get you items that will help but aren't necessarily needed. (Overworld helper items). Could be something like throwing daggers for nearby animals that crawl around so you can eat them for food or something. Animals will show up everywhere. Ehhh, maybe they'll just spit out hearts for health.
+
+There will be a beacon over the boss room of the dungeon (you have to find the village yourself).
+
+The village's race could be something you don't have a transformation for, so you wouldn't be able to complete nor start the dungeon.
+
+If the dungeon contains a transformation, then the villagers will be talking about how "a X boy/girl went into the dungeon and they haven't come out in days!"
+
+What the villagers talk about, the key item, and the centerpiece of the dungeon are tied together.
+
+Sometimes, a certain villager will recognize your transformation and an emotional scene will trigger (they will always go into the dungeon and help you... though if your skill level is low then they'll die. Or maybe that'll be part of the story)
+
+
+
 Each dungeon type will have 5 dungeons for a total of 15 dungeons.
 
-Some will have an npc that tells you about the skill needed to get into the dungeon.
+Some will have an npc that tells you about the skill needed to get into the dungeon. And then the npc guides you in the first half and gets captured in the next half idk.
 
 Some will have just an obstacle that will make the skill check required.
 
 Maybe at this point having the dungeons not be randomly generated could be good to pull down the complexity. Perhaps just the overworld being random gen would be the best.
 
-Each dungeon should be accessible to get the first item if the dungeon gets completed and gets their overworld outside structure changes (usu. opening up a path to another area) bc the player could lose the item upon dying.
+Each dungeon should be accessible to get the first item if the dungeon gets completed and gets their overworld outside structure changed (usu. opening up a path to another area) bc the player could lose the item upon dying.
 
 
 
