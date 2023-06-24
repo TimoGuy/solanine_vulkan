dd3df29e28224590504ccb9518a7357a0de3ce22
TimoGuy <timoguy99004@gmail.com>
2023-06-24 11:18:38 -0700

    Smol commit to stub out the voxel field lighting grid id placement.

--- solanine_vulkan/src/RenderObject.h
+++ solanine_vulkan/src/RenderObject.h
@@ -22,7 +22,7 @@ struct GPUInstancePointer
 	uint32_t objectID;
 	uint32_t materialID;
 	uint32_t animatorNodeID;
-	uint32_t pad;
+	uint32_t voxelFieldLightingGridID;  // @INCOMPLETE
 };
 
 enum class RenderLayer
